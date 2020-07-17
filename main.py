49f73aadcb475b9d628bad4605348814649cc26d
thejduman <thejduman@yahoo.com>
2020-07-17 17:03:21 +0000

    part 2

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+#part 1
+#get input from user
 userInput = str(input("Enter a phrase: "))
+#convert to lower and print
 newInput = userInput.lower()
 print(newInput)
+
+#part 2
+#create vowel list
+vowels = ["a", "e", "i", "o", "u"]
+for i in newInput:
+  if i in vowels: #check the vowel list
+    print(i + " is a vowel")
+  else:
+    print(i + " is a consonant")
+
+
