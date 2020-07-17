30d683e4576aefbdd88beefd4ae6299021c6575b
thejduman <thejduman@yahoo.com>
2020-07-17 17:27:20 +0000

    part 3

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ print(newInput)
 #part 2
 #create vowel list
 vowels = ["a", "e", "i", "o", "u"]
+#variables for tracking vowels and consonants
+numVowels = 0
+numConsonants = 0
 for i in newInput:
   if i in vowels: #check the vowel list
     print(i + " is a vowel")
-  else:
+    numVowels = numVowels + 1
+  else: #otherwise it's a consonant
     print(i + " is a consonant")
-
-
+    numConsonants = numConsonants + 1
+#print vowels and consonants
+print("# of vowels: " + str(numVowels))
+print("# of consonants: " + str(numConsonants))
